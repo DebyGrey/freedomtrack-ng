5f7fe5b8de7ec9060886fb271b38c2fd45aedfdb
DebyGrey <demifashoro@gmail.com>
2025-06-02 05:48:04 +0100

    Fix session

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@ echo "Running composer"
 
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Running migrations..."
+php artisan migrate --force
+php artisan db:seed --force
+
 echo "Caching config..."
 php artisan config:cache
 
